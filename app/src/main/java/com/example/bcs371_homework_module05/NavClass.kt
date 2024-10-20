563e5219e415efbd6809b4cef3e420a12efacb36
Justin Sam <jusitnsam02544@gmail.com>
2024-10-19 20:47:12 -0400

    saving

--- app/src/main/java/com/example/bcs371_homework_module05/NavClass.kt
+++ app/src/main/java/com/example/bcs371_homework_module05/NavClass.kt
@@ -2,7 +2,6 @@ package com.example.bcs371_homework_module05
 
 import android.view.animation.OvershootInterpolator
 import androidx.compose.animation.core.Animatable
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,16 +15,22 @@ import androidx.navigation.compose.rememberNavController
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
+import androidx.compose.material3.RadioButton
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
+import androidx.compose.material3.MaterialTheme
 
 
 @Composable
@@ -42,6 +47,7 @@ fun Navigation(){
     }
 }
 
+//SplashScreen
 @Composable
 fun SplashScreen(navController: NavController){
     val scale = remember {
@@ -58,6 +64,7 @@ fun SplashScreen(navController: NavController){
         navController.navigate("first_screen")
     }
 
+    //Centers the text and image
     Column(horizontalAlignment = Alignment.CenterHorizontally){
         Image(painter = painterResource(id = R.drawable.dollar_sign), contentDescription ="")
         Text("Who wants to be a millionaire")
@@ -65,17 +72,51 @@ fun SplashScreen(navController: NavController){
 }
 
 @Composable
-fun FirstScreen(navController: NavController){
-    Box(){
-        Column()
-        {
-            Text("Homework",
+fun FirstScreen(navController: NavController) {
+    val radioOptions = listOf("50", "61", "31", "21")
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[1]) }
+    Column(verticalArrangement = Arrangement.Top,
+           horizontalAlignment = Alignment.CenterHorizontally,
+           modifier = Modifier.fillMaxSize()
+               .background(Color.Black)
+    ) {
+
+        Text(
+            "Homework", Modifier
+                .background(Color.Black)
+                .padding(20.dp),
+            color = Color.White
+        )
+        
+        Text("First Question:")
+        Text("How many states does US have?")
+        Spacer(modifier = Modifier.padding(10.dp))
+        radioOptions.forEach { text ->
+            Row(
                 Modifier
-                    .background(Color.Black)
-                    .padding(20.dp)
-                    .align(Alignment.Start),
-                color = Color.White
-            )
+
+                    .selectable(
+                        selected = (text == selectedOption),
+                        onClick = {
+                            onOptionSelected(text)
+                        }
+                    )
+                    .padding(horizontal = 16.dp)
+            ) {
+                RadioButton(
+                    selected = (text == selectedOption),
+                    onClick = { onOptionSelected(text) }
+                )
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.bodyLarge.merge(),
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.padding(30.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text("Confirm")
         }
     }
 }
